86fe51fe2b28cf7c8cde33499b7a822f7862edaf
Kenley Bides <kenbides1@gmail.com>
2025-04-10 11:29:40 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// dynamically insert the name in html file
+const studentInfo = document.getElementById("student-info");
+studentInfo.textContent = "Kenley Bides - Student ID: 200607233";
+
+// calls the cat api
+const catBtn = document.getElementById("load-cat");
+const catContainer = document.getElementById("cat-container");
+
+catBtn.addEventListener("click", () => {
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      const catImg = document.createElement("img");
+      catImg.src = data[0].url;
+      catImg.alt = "A cute cat";
+      catImg.classList.add("cat-img");
+
+      catContainer.innerHTML = ""; // reset (clears the previous cat)
+      catContainer.appendChild(catImg);
+    })
+    .catch((error) => {
+      catContainer.innerHTML = "<p>Couldn't load a cat 😿</p>";
+      console.error("Error fetching cat image:", error);
+    });
+});
